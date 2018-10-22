2bced84604f5ba6ed33dd5226db413dbd1522613
Anette Olli Siiri <anettosi@stud.ntnu.no>
2018-10-22 23:56:40 +0200

    added sample config file

---
+++ dao/configSample.js
@@ -0,0 +1,9 @@
+module.exports = {
+  connectionLimit: 10,
+  host: "yourHost",
+  user: "user",
+  password: "yourPassword,",
+  database: "YourDatabase",
+  debug: false,
+  multipleStatements: true
+};
