03472c2beef6d0f34a479f333e652e4f24be7b1c
Anette Olli Siiri <anettosi@stud.ntnu.no>
2018-11-16 18:34:37 +0100

    fikset feilende tester

--- server/test/dao/create_testdata.sql
+++ server/test/dao/create_testdata.sql
@@ -1,11 +1,8 @@
-INSERT INTO Kategori VALUE('agurknytt');
-INSERT INTO Kategori VALUE ('internet-of-shit');
+INSERT INTO Kategori VALUE('javascript');
+INSERT INTO Kategori VALUE ('java');
 INSERT INTO Kategori VALUE ('matlaging');
 INSERT INTO Kategori VALUE ('sjdkfl');
 INSERT INTO Artikler (tittel, kategori, innhold, bildeLink, bildeTekst, isViktig) VALUE ('hest er best', 'matlaging', 'lage hestepolse i dag', 'www.hest.no', 'bilde av en hest', 0);
 INSERT INTO Artikler (tittel, kategori, innhold, bildeLink, bildeTekst, isViktig) VALUE ('javascript programmering', 'javascript', 'blakjadkljgagjkalkgj', 'www.akdgja.com', 'akdfjkjagd', 1 );
 
 INSERT INTO rating_artikler VALUE (1, 4), (2, 3), (1, 5);
-
-
-SELECT Artikler.tittel, Artikler.innhold, Artikler.bildeLink, Artikler.bildeTekst, Artikler.isViktig, AVG(rating_artikler.rating) AS avgRating, DATE_FORMAT(Artikler.tidspunktOpprettet, '%Y-%m-%d %H:%i') AS opprettet, DATE_FORMAT(Artikler.tidspunktEndret, '%Y-%m-%d %H:%i') AS endret  FROM Artikler LEFT JOIN rating_artikler ON(Artikler.id = rating_artikler.id) GROUP BY Artikler.id ORDER BY avgRating DESC;
